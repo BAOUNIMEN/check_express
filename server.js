6fa3080179436f5832bd424e2e78db70ee181119
BAOUNIMEN <benounimen91@gmail.com>
2023-05-14 22:34:37 +0200

    commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const PORT = 5000;
+
+
+// GET method 
+
+app.use(function (req, res, next) {
+    const date = new Date();
+    const hours = date.getHours();
+    const day = date.getDay()
+    if(hours >9 && hours <18 && day<6 && day>0){
+        next();
+    }else{
+        res.send("This website  work from Monday to Friday 9am to 5pm")
+    }
+});
+
+app.use(express.static('public'))
+
+
+
+
+app.listen(PORT,err=>err? console.log(err):console.log(`Server is running on port ${PORT}`))
