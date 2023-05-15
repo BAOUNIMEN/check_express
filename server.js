61aeae0478f7bbc2ac3b055c39a0c39cea23db2f
BAOUNIMEN <benounimen91@gmail.com>
2023-05-15 14:25:10 +0200

    commit

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ app.use(function (req, res, next) {
     const date = new Date();
     const hours = date.getHours();
     const day = date.getDay()
-    if(hours >9 && hours <18 && day<6 && day>0){
+    if(hours >9 && hours <18 && day<6 && day>1){
         next();
     }else{
         res.send("This website  work from Monday to Friday 9am to 5pm")
     }
 });
 
-app.use(express.static('public'))
+app.use(express.static('test'))
 
 
 
